a421263e1bf217c89777d601ac373cedf76c0523
TheRealBilaal <42493384+TheRealBilaal@users.noreply.github.com>
2019-10-03 12:29:34 +0100

    Creates main class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+/**
+ * The main program that returns the data of a person from their email id
+ */
 public class Main {
 
+    /**
+     * Gets information about a person from their email id
+     * @param args The command line arguments
+     */
     public static void main(String[] args) {
-	// write your code here
+
     }
 }
