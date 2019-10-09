8f2236d6e1521fb50c81226dffad5824f1900c65
TheRealBilaal <42493384+TheRealBilaal@users.noreply.github.com>
2019-10-09 10:33:54 +0100

    Refactored user messages into seperate Strings class

--- src/Main.java
+++ src/Main.java
@@ -5,37 +5,13 @@ import java.util.Scanner;
  */
 public class Main {
 
-    // Constants
-
-    /**
-     * The message to display when asking the user for an input
-     */
-    private static final String INPUT_MESSAGE = "Username: ";
-
-    /**
-     * The message to display when an error occurs
-     */
-    private static final String ERROR_MESSAGE = "There was an error.";
-
-    /**
-     * The message to display when outputting the person's full name
-     */
-    private static final String FULL_NAME_MESSAGE = "Name: ";
-
-    /**
-     * The message to display when outputting the person's telephone number
-     */
-    private static final String TELEPHONE_MESSAGE = "Telephone: ";
-
-    // Methods
-
     /**
      * Gets information about a person from their email id
      * @param args The command line arguments
      */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print(INPUT_MESSAGE);
+        System.out.print(Strings.INPUT_MESSAGE);
         String username = scanner.next();
         scanner.close();
 
@@ -43,14 +19,15 @@ public class Main {
         Person outputPerson = LookupController.lookupPerson(inputPerson);
 
         if (outputPerson == null || outputPerson.getFullname() == null || outputPerson.getTelephone() == null) {
-            System.out.println(ERROR_MESSAGE);
+            System.out.println(Strings.ERROR_MESSAGE);
             System.exit(0);
         }
 
         String name = outputPerson.getFullname();
-        System.out.println(FULL_NAME_MESSAGE + name);
+        System.out.println(Strings.FULL_NAME_MESSAGE + name);
 
         String telephone = outputPerson.getTelephone();
-        System.out.println(TELEPHONE_MESSAGE + telephone);
+        System.out.println(Strings.TELEPHONE_MESSAGE + telephone);
     }
+
 }
---
+++ src/Strings.java
@@ -0,0 +1,26 @@
+/**
+ * Manages the string messages to output to the user
+ */
+class Strings {
+
+    /**
+     * The message to display when asking the user for an input
+     */
+    static final String INPUT_MESSAGE = "Username: ";
+
+    /**
+     * The message to display when an error occurs
+     */
+    static final String ERROR_MESSAGE = "There was an error.";
+
+    /**
+     * The message to display when outputting the person's full name
+     */
+    static final String FULL_NAME_MESSAGE = "Name: ";
+
+    /**
+     * The message to display when outputting the person's telephone number
+     */
+    static final String TELEPHONE_MESSAGE = "Telephone: ";
+
+}
