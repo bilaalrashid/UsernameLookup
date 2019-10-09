1526bc697014fbc8d5dc649e41361dee17ccbcbe
TheRealBilaal <42493384+TheRealBilaal@users.noreply.github.com>
2019-10-09 14:52:19 +0100

    Refactored the Main class

--- src/Main.java
+++ src/Main.java
@@ -10,27 +10,59 @@ public class Main {
      * @param args The command line arguments
      */
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print(Strings.INPUT_MESSAGE);
-        String username = scanner.next();
-        scanner.close();
-
+        String username = inputUsername();
         Person inputPerson = new Person(username);
+
         Person outputPerson = LookupController.lookupPerson(inputPerson);
 
-        if (outputPerson == null || outputPerson.getFullname() == null || outputPerson.getTelephone() == null || outputPerson.getEmail() == null) {
+        if (checkForError(outputPerson)) {
             System.out.println(Strings.ERROR_MESSAGE);
             System.exit(0);
         }
 
-        String name = outputPerson.getFullname();
+        outputDetails(outputPerson);
+    }
+
+    /**
+     * Prompts the user to input a username
+     * @return The inputted username
+     */
+    private static String inputUsername() {
+        System.out.print(Strings.INPUT_MESSAGE);
+
+        Scanner scanner = new Scanner(System.in);
+        String username = scanner.next();
+        scanner.close();
+
+        return username;
+    }
+
+    /**
+     * Checks if there was an error in the process
+     * @param person The outputted person
+     * @return If an error is detected
+     */
+    private static boolean checkForError(Person person) {
+        return person == null
+                || person.getFullname() == null
+                || person.getTelephone() == null
+                || person.getEmail() == null;
+    }
+
+    /**
+     * Outputs the fetched details to the user
+     * @param person The person to output details about
+     */
+    private static void outputDetails(Person person) {
+        String name = person.getFullname();
         System.out.println(Strings.FULL_NAME_MESSAGE + name);
 
-        String telephone = outputPerson.getTelephone();
+        String telephone = person.getTelephone();
         System.out.println(Strings.TELEPHONE_MESSAGE + telephone);
 
-        String email = outputPerson.getEmail();
+        String email = person.getEmail();
         System.out.println(Strings.EMAIL_MESSAGE + email);
     }
+
 
 }
